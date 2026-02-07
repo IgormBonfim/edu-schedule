13335b924a5d8bd0dd36017f9a9d5a19d6f022b7
Igor Bonfim <igormeirabonfim@gmail.com>
2026-02-07 20:13:55 -0300

    Implementa configuracao do Graph

---
+++ backend/EduSchedule.Ioc/Extensions/ConfigureGraph.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Graph;
+
+namespace EduSchedule.Ioc.Extensions
+{
+    public static class ConfigureGraph
+    {
+        public static IServiceCollection AddGraph(this IServiceCollection services, IConfiguration configuration)
+        {
+            var azureAdConfig = configuration.GetSection("AzureAd");
+            var tenantId = configuration["AzureAd:TenantId"];
+
+            if (string.IsNullOrEmpty(tenantId))
+            {
+                throw new InvalidOperationException("A configuração 'AzureAd:TenantId' não foi injetada corretamente.");
+            }
+
+            var credential = new ClientSecretCredential(
+                azureAdConfig["TenantId"],
+                azureAdConfig["ClientId"],
+                azureAdConfig["ClientSecret"]
+            );
+
+            services.AddScoped(sp => new GraphServiceClient(credential));
+
+            return services;
+        }
+    }
+}
