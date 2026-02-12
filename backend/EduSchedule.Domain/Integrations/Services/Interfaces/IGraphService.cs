a34e4338c0be965e6d90a0192ef2e1ec63d109b8
Igor Bonfim <igormeirabonfim@gmail.com>
2026-02-11 23:25:23 -0300

    Ajusta interface IGraphService

--- backend/EduSchedule.Domain/Integrations/Services/Interfaces/IGraphService.cs
+++ backend/EduSchedule.Domain/Integrations/Services/Interfaces/IGraphService.cs
@@ -6,6 +6,6 @@ namespace EduSchedule.Domain.Integrations.Services.Interfaces
     {
         Task<DeltaResults<UserResult>> GetUsersDeltaAsync(string? deltaLink = null, int top = 999, CancellationToken cancellationToken = default);
         Task<UserResult?> GetUserAsync(string userId, CancellationToken cancellationToken = default);
-        Task<DeltaResults<EventResult>> GetUserEventsDeltaAsync(string studentId, string? deltaLink, int top = 999, CancellationToken cancellationToken = default);
+        Task<DeltaResults<EventResult>> GetUserEventsDeltaAsync(string studentId, string? deltaLink = null, CancellationToken cancellationToken = default);
     }
 }
